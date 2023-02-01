b7326988561841577417a83e96185e3887c01585
Jaga <jagadheesh6@gmail.com>
2023-02-01 20:10:10 +0530

    Initial Commit

--- src/api/users/model/users.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.UserSchema = void 0;
-var mongoose_1 = require("mongoose");
-exports.UserSchema = new mongoose_1.Schema({
-    name: String,
-    email: String
-});
--- src/core/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.TYPES = void 0;
-exports.TYPES = {
-    ///Models
-    UsersModel: Symbol["for"]('UsersModel'),
-    ///Connection
-    DatabaseConnection: Symbol["for"]('DatabaseConnection')
-};

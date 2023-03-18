2f72a6045ca0ff9363c259ddc76db8ae33289dfd
jaga-live <jagadheesh6@gmail.com>
2023-03-18 17:52:23 +0530

    fix - AuthGuard

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
-import { AuthGuard, PassportModule } from '@nestjs/passport';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from 'src/api/auth/controllers/auth.controller';
+import { AuthGuard } from 'src/api/auth/guards/auth.guard';
 import { authProviders } from 'src/api/auth/model/auth.provider';
 import { AuthService } from 'src/api/auth/service/auth.service';
 import { KittychanModule } from 'src/api/kitty_chan/kitty_chan.module';

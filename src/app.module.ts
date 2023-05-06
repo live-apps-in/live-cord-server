2077089a06320fd5b9d533b7bf805f1c63798d33
Jaga <jagadheesh6@gmail.com>
2023-05-06 10:42:44 +0530

    fix - possible env fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { KittychanModule } from 'src/modules/kitty_chan/kitty_chan.module';
 import { UserModule } from 'src/modules/users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, KittychanModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    KittychanModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
